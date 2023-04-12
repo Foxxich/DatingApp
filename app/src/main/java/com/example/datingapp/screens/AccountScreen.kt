31c54933238ef1673a3425baa792500ffbcb8315
Foxxich <64288356+Foxxich@users.noreply.github.com>
2023-04-12 02:18:50 +0200

    Updated compose for messages and account

--- app/src/main/java/com/example/datingapp/screens/AccountScreen.kt
+++ app/src/main/java/com/example/datingapp/screens/AccountScreen.kt
@@ -1,15 +1,23 @@
 package com.example.datingapp.screens
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import coil.compose.AsyncImage
 import com.example.datingapp.compose.BottomBar
 import com.example.datingapp.ui.theme.backgroundColor
+import com.example.datingapp.ui.theme.whiteColor
 
 class AccountScreen: ScreenController {
 
@@ -17,12 +25,60 @@ class AccountScreen: ScreenController {
     override fun Prepare(navController: NavHostController) {
         Scaffold(bottomBar = { BottomBar().Prepare(navController) }
         ) {
-            Box(
+            Surface(
                 modifier = Modifier
-                    .background(backgroundColor)
-                    .fillMaxSize()
+                    .fillMaxSize(),
+                color = backgroundColor
             ) {
-                Text("Account Screen")
+                Column(modifier = Modifier
+                    .fillMaxHeight(0.9f)
+                    .fillMaxWidth()
+                    .padding(10.dp)) {
+                    AsyncImage(
+                        model = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d8/Person_icon_BLACK-01.svg/1924px-Person_icon_BLACK-01.svg.png",
+                        contentDescription = "Translated description of what the image contains",
+                        modifier = Modifier
+                            .background(whiteColor)
+                            .clip(RoundedCornerShape(10.dp))
+                            .fillMaxWidth()
+                    )
+                    Divider(
+                        color = Color.LightGray,
+                        thickness = 1.dp,
+                        modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp)
+                    )
+                    Box(
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(10.dp))
+                            .background(whiteColor)
+                            .fillMaxWidth()
+                    ) {
+                        Box() {
+
+                        }
+                        Row(
+                            horizontalArrangement = Arrangement.SpaceEvenly,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Button(
+                                onClick = { /* do something */ },
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .padding(8.dp)
+                            ) {
+                                Text("Button 1")
+                            }
+                            Button(
+                                onClick = { /* do something else */ },
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .padding(8.dp)
+                            ) {
+                                Text("Button 2")
+                            }
+                        }
+                    }
+                }
             }
         }
     }
