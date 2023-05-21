7cc68273da41a901f2e4f346538c38487c025f5e
Foxxich <64288356+Foxxich@users.noreply.github.com>
2023-05-21 15:33:20 +0200

    Removed dependencies

--- app/src/androidTest/java/com/example/datingapp/StartActivityTest.kt
+++ app/src/androidTest/java/com/example/datingapp/StartActivityTest.kt
@@ -9,8 +9,6 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import com.example.datingapp.activities.SignActivity
 import com.example.datingapp.activities.StartActivity
-import com.example.datingapp.firebase.FirebaseAuthController
-import com.example.datingapp.firebase.FirebaseAuthControllerImpl
 import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
@@ -22,16 +20,15 @@ class StartActivityTest {
     @get:Rule
     val composeTestRule = createAndroidComposeRule<StartActivity>()
 
-    private var firebaseAuthController: FirebaseAuthController = FirebaseAuthControllerImpl()
     private val context: Context = InstrumentationRegistry.getInstrumentation().targetContext
 
     @Test
     fun newUserLogin() {
-        firebaseAuthController.logout()
         val activityMonitor = Instrumentation.ActivityMonitor(SignActivity::class.java.name, null, false)
         InstrumentationRegistry.getInstrumentation().addMonitor(activityMonitor)
         composeTestRule.onNodeWithContentDescription(context.getString(R.string.heart_image_description)).performClick()
         Thread.sleep(2000)
+        println("XDD " + activityMonitor.lastActivity)
         assertEquals(activityMonitor.lastActivity::class, SignActivity::class)
         InstrumentationRegistry.getInstrumentation().removeMonitor(activityMonitor)
     }
