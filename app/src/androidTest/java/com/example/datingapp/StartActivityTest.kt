fbf165539da012751552d1f5986fd16fa31e828b
Foxxich <64288356+Foxxich@users.noreply.github.com>
2023-05-30 15:45:27 +0200

    Added simple test to catch if user logging works ax expected

--- app/src/androidTest/java/com/example/datingapp/StartActivityTest.kt
+++ app/src/androidTest/java/com/example/datingapp/StartActivityTest.kt
@@ -7,8 +7,12 @@ import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import com.example.datingapp.activities.SetUpActivity
 import com.example.datingapp.activities.SignActivity
 import com.example.datingapp.activities.StartActivity
+import com.example.datingapp.firebase.FirebaseAuthController
+import com.example.datingapp.firebase.FirebaseAuthControllerImpl
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
@@ -22,6 +26,8 @@ class StartActivityTest {
 
     private val context: Context = InstrumentationRegistry.getInstrumentation().targetContext
 
+    private val firebaseAuthController: FirebaseAuthController = FirebaseAuthControllerImpl()
+
     @Test
     fun newUserLogin() {
         val activityMonitor = Instrumentation.ActivityMonitor(SignActivity::class.java.name, null, false)
@@ -30,6 +36,20 @@ class StartActivityTest {
         Thread.sleep(2000)
         assertEquals(activityMonitor.lastActivity::class, SignActivity::class)
         InstrumentationRegistry.getInstrumentation().removeMonitor(activityMonitor)
+    }
+
+    @Test
+    fun oldUserLogin() {
+        runBlocking {
+            firebaseAuthController.isCurrentUserRegistered("test1@gmail.com", "123456")
+        }
+        val activityMonitor = Instrumentation.ActivityMonitor(SetUpActivity::class.java.name, null, false)
+        InstrumentationRegistry.getInstrumentation().addMonitor(activityMonitor)
+        composeTestRule.onNodeWithContentDescription(context.getString(R.string.heart_image_description)).performClick()
+        Thread.sleep(2000)
+        assertEquals(activityMonitor.lastActivity::class, SetUpActivity::class)
+        InstrumentationRegistry.getInstrumentation().removeMonitor(activityMonitor)
+        firebaseAuthController.logout()
     }
 
 }
